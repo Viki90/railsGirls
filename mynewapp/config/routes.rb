bb7a9c0ec58bff4e6a346f6689d52cb2ebf4875a
Viktorija <viki.verebes90@gmail.com>
2017-11-19 15:07:15 +0100

    preusmeravanje

--- mynewapp/config/routes.rb
+++ mynewapp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :ideas
   get 'pages/info'
+  root to: redirect('/ideas')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
